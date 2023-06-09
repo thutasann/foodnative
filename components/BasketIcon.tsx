181fd568aac064fe316d99c6f6557dd19f0aa43d
Thuta San <thutasann2002@gmail.com>
2023-06-09 09:50:08 +0630

    ui: basket popup

---
+++ components/BasketIcon.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Currency from 'react-currency-formatter'
+import { useSelector } from 'react-redux'
+import { selectBasketItems, selectBasketTotal } from '../slices/basketSlice'
+import { useNavigation } from '@react-navigation/native'
+import { StyledText, StyledTouchableOpacity, StyledView } from '../commons'
+
+const BasketIcon = () => {
+  const items: any[] = useSelector(selectBasketItems)
+  const navigation = useNavigation()
+  const basketTotal = useSelector(selectBasketTotal)
+  return (
+    <StyledView className='absolute bottom-10 w-full z-50'>
+      <StyledTouchableOpacity className='mx-5 bg-[#00CCBB] shadow-md p-4 rounded-lg flex-row items-center space-x-1'>
+        <StyledText className='text-white font-extrabold text-lg bg-[#01A296] py-1 px-2'>{items?.length}</StyledText>
+        <StyledText className='flex-1 text-white font-extrabold text-lg text-center'>View Basket</StyledText>
+        <StyledText className='text-lg text-white font-extrabold'>
+          <Currency quantity={basketTotal} currency='USD' />
+        </StyledText>
+      </StyledTouchableOpacity>
+    </StyledView>
+  )
+}
+
+export default BasketIcon
--- slices/basketSlice.ts
+++ slices/basketSlice.ts
@@ -28,11 +28,13 @@ export const basketSlice = createSlice({
 })
 
 export const { addToBasket, removeFromBasket } = basketSlice.actions
+export default basketSlice.reducer
+
 export const selectBasketItems = (state: any) => state.basket.items
 
 export const selectBasketItemWithId = (state: any, id: string) => state.basket.items?.filter((item: any) => item.id === id)
 
-export default basketSlice.reducer
+export const selectBasketTotal = (state: any) => state.basket.items?.reduce((total: any, item: any) => (total += item.price), 0)
 
 // I Know I need to implement with TypeScript
 // but skip this part cauz I was lazy XD
